3164b177470462dbcd087742b75f095b25aa7c10
Josh Weaver <jtw@digitalartbrokers.io>
2023-01-31 15:40:47 -0600

    Editing and improving upon the Kanban SSH server

---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type status int
+
+const divisor = 4
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+/* STYLING */
+var (
+	columnStyle = lipgloss.NewStyle().
+			Padding(1, 2)
+	focusedStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62"))
+	helpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241"))
+)
+
+/* CUSTOM ITEM */
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+//implement the list.Item interface
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+/* MAIN MODEL */
+
+type Model struct {
+	focused status
+	lists   []list.Model
+	err     error
+	loaded  bool
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+// TODO: call this on tea.WindowSizeMsg
+func (m *Model) initLists(width, height int) {
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height)
+	defaultList.SetShowHelp(false)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+
+	//Init To Do
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "spicy california roll, crab roll"},
+		Task{status: todo, title: "fold laudrey", description: "or wear wrinkly tshirts"},
+	})
+
+	//Init in progress
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{status: todo, title: "stay cool", description: "as a cucumber"},
+	})
+
+	//Init Done
+	m.lists[done].Title = "Done"
+	m.lists[done].SetItems([]list.Item{
+		Task{status: todo, title: "stay cool", description: "as a cucumber"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	//Message when the terminal boots up that says its size
+	case tea.WindowSizeMsg:
+		if !m.loaded {
+			m.initLists(msg.Width, msg.Height)
+			m.loaded = true
+		}
+	}
+	var cmd tea.Cmd
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	if m.loaded {
+		todoView := m.lists[todo].View()
+		inProgView := m.lists[todo].View()
+		doneView := m.lists[todo].View()
+		switch m.focused {
+		default:
+			return lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				focusedStyle.Render(todoView),
+				columnStyle.Render(inProgView),
+				columnStyle.Render(doneView),
+			)
+		}
+	} else {
+		return "loading..."
+	}
+
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
